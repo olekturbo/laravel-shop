bf7107e82e102854e39a41d8185e40f2ec7dabfc
olekturbo <aleks.szewczak@gmail.com>
2018-12-24 02:40:19 +0100

    Change tpay controller

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -3,33 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+
 
 class TransferController extends Controller
 {
-    public function callback(Request $request) {
+    public function callback() {
         // sprawdzenie adresu IP oraz występowania zmiennych POST
         $ip_table = array('195.149.229.109', '148.251.96.163', '178.32.201.77',
-            '46.248.167.59', '46.29.19.106'); $request['id'];
-        if (in_array($_SERVER['REMOTE_ADDR'], $ip_table) && !empty($request)){
-            $id_sprzedawcy = $request['id'];
-            $status_transakcji = $request['tr_status'];
-            $id_transakcji = $request['tr_id'];
-            $kwota_transakcji = $request['tr_amount'];
-            $kwota_zaplacona = $request['tr_paid'];
-            $blad = $request['tr_error'];
-            $data_transakcji = $request['tr_date'];
-            $opis_transakcji = $request['tr_desc'];
-            $ciag_pomocniczy = $request['tr_crc'];
-            $email_klienta = $request['tr_email'];
-            $suma_kontrolna = $request['md5sum'];
+            '46.248.167.59', '46.29.19.106'); $_POST['id'];
+        if (in_array($_SERVER['REMOTE_ADDR'], $ip_table) && !empty($_POST)){
+            $id_sprzedawcy = $_POST['id'];
+            $status_transakcji = $_POST['tr_status'];
+            $id_transakcji = $_POST['tr_id'];
+            $kwota_transakcji = $_POST['tr_amount'];
+            $kwota_zaplacona = $_POST['tr_paid'];
+            $blad = $_POST['tr_error'];
+            $data_transakcji = $_POST['tr_date'];
+            $opis_transakcji = $_POST['tr_desc'];
+            $ciag_pomocniczy = $_POST['tr_crc'];
+            $email_klienta = $_POST['tr_email'];
+            $suma_kontrolna = $_POST['md5sum'];
 
             if($status_transakcji=='TRUE' && $blad=='none'){
-               Log::info('true');
+
             }
             else
             {
-              Log::info('false');
+
             }
         }
         echo 'TRUE'; // odpowiedź dla serwera o odebraniu danych
