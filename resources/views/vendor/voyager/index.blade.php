625123707c43cd670e275946198a50321b077760
olekturbo <aleks.szewczak@gmail.com>
2018-12-26 00:57:22 +0100

    Dimmers removed

---
+++ resources/views/vendor/voyager/index.blade.php
@@ -0,0 +1,12 @@
+@extends('voyager::master')
+
+@section('content')
+    <div class="page-content">
+        @include('voyager::alerts')
+        <div class="row">
+            <div class="col-md-12">
+                Witaj w panelu MyOwn!
+            </div>
+        </div>
+    </div>
+@stop
