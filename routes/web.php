bde5cc94e13a5bd13f3a4526cc9d1986de34bf21
olekturbo <aleks.szewczak@gmail.com>
2018-12-26 02:02:20 +0100

    Change routes

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,17 @@
 */
 
 Route::get('/', 'WelcomeController@index')->name('welcome');
-Route::get('/home', 'WelcomeController@index')->name('welcome');
-
-Route::get('/category/{category}/product/{id}/{slug?}', 'ProductController@show')->name('product');
-Route::get('/cart', 'ProductController@showCart')->name('product.showCart');
-Route::post('/add-to-cart/{id}', 'ProductController@addToCart')->name('product.addToCart');
-Route::get('/update-cart/{id}/{size}', 'ProductController@updateCart')->name('product.updateCart');
-Route::delete('/delete-from-cart/{id}/{size}', 'ProductController@deleteFromCart')->name('product.deleteFromCart');
-Route::get('/order/new', 'ProductController@order')->name('product.order');
+Route::get('/kategoria/{category}/produkt/{id}/{slug?}', 'ProductController@show')->name('product');
+Route::get('/koszyk', 'ProductController@showCart')->name('product.showCart');
+Route::post('/dodaj-do-koszyka/{id}', 'ProductController@addToCart')->name('product.addToCart');
+Route::get('/aktualizuj-koszyk/{id}/{size}', 'ProductController@updateCart')->name('product.updateCart');
+Route::delete('/usun-z-koszyka/{id}/{size}', 'ProductController@deleteFromCart')->name('product.deleteFromCart');
+Route::get('/zamow/nowy', 'ProductController@order')->name('product.order');
 Route::post('/transfer/order/new', 'TransferController@order')->name('transfer.order');
 Route::post('/transfer/order/callback', 'TransferController@callback')->name('transfer.callback');
 Route::get('/transfer/order/success', 'TransferController@success')->name('transfer.success');
 Route::get('/transfer/order/error', 'TransferController@error')->name('transfer.error');
-Route::get('/category/{category}', 'CategoryController@show')->name('category');
+Route::get('/kategoria/{category}', 'CategoryController@show')->name('category');
 
 Auth::routes(['verify' => 'true']);
 
