234c0a1c84d67aa85aab06d6e406ceb9847f664b
olekturbo <aleks.szewczak@gmail.com>
2018-12-29 15:17:37 +0100

    Update social for google

--- config/services.php
+++ config/services.php
@@ -53,4 +53,10 @@ return [
         'redirect'      => env('GITHUB_URL'),
     ],
 
+    'google' => [
+        'client_id'     => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect'      => env('GOOGLE_URL'),
+    ],
+
 ];
