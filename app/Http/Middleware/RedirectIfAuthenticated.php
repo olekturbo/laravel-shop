853b0697d2d25028f48684e0cef2f84b0609f436
olekturbo <aleks.szewczak@gmail.com>
2018-12-29 18:45:10 +0100

    Small fixes

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,11 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        if(Auth::user() && !is_null(Auth::user()->provider && is_null(Auth::user()->city)))
+            return redirect()->route('provider.fill-data');
+
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return redirect('/');
         }
 
         return $next($request);
