58a4a946042969d3df7f72eac796f38f01d8c903
olekturbo <aleks.szewczak@gmail.com>
2018-12-29 02:35:06 +0100

    edited controller

--- app/Http/Controllers/Auth/SocialAccountController.php
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -2,43 +2,134 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\SocialAccountService;
-use Illuminate\Http\Request;
+use App\User;
+use Exception;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialAccountController extends Controller
 {
-    /**
-     * Redirect the user to the GitHub authentication page.
-     *
-     * @return Response
-     */
-    public function redirectToProvider($provider)
+    public function __construct()
     {
-        return \Socialite::driver($provider)->redirect();
+        $this->middleware('guest');
     }
 
     /**
-     * Obtain the user information
+     * List of providers configured in config/services acts as whitelist
      *
-     * @return Response
+     * @var array
      */
-    public function handleProviderCallback(SocialAccountService $accountService, $provider)
-    {
+    protected $providers = [
+        'github',
+        'facebook',
+        'google',
+        'twitter'
+    ];
 
-        try {
-            $user = \Socialite::with($provider)->user();
-        } catch (\Exception $e) {
-            return redirect()->route('login');
+
+    /**
+     * Redirect to provider for authentication
+     *
+     * @param $driver
+     * @return mixed
+     */
+    public function redirectToProvider($driver)
+    {
+        if( ! $this->isProviderAllowed($driver) ) {
+            return $this->sendFailedResponse("{$driver} is not currently supported");
         }
 
-        $authUser = $accountService->findOrCreate(
-            $user,
-            $provider
-        );
+        try {
+            return Socialite::driver($driver)->redirect();
+        } catch (Exception $e) {
+            // You should show something simple fail message
+            return $this->sendFailedResponse($e->getMessage());
+        }
+    }
 
-        auth()->login($authUser, true);
+    /**
+     * Handle response of authentication redirect callback
+     *
+     * @param $driver
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function handleProviderCallback( $driver )
+    {
+        try {
+            $user = Socialite::driver($driver)->user();
+        } catch (Exception $e) {
+            return $this->sendFailedResponse($e->getMessage());
+        }
 
-        return redirect()->route('welcome');
+        // check for email in returned user
+        return empty( $user->email )
+            ? $this->sendFailedResponse("No email id returned from {$driver} provider.")
+            : $this->loginOrCreateAccount($user, $driver);
+    }
+
+    /**
+     * Send a successful response
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendSuccessResponse()
+    {
+        return redirect()->intended('home');
+    }
+
+    /**
+     * Send a failed response with a msg
+     *
+     * @param null $msg
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendFailedResponse($msg = null)
+    {
+        return redirect()->route('social.login')
+            ->withErrors(['msg' => $msg ?: 'Unable to login, try with another provider to login.']);
+    }
+
+    protected function loginOrCreateAccount($providerUser, $driver)
+    {
+        // check for already has account
+        $user = User::where('email', $providerUser->getEmail())->first();
+
+        // if user already found
+        if( $user ) {
+            // update the avatar and provider that might have changed
+            $user->update([
+                'avatar' => $providerUser->avatar,
+                'provider' => $driver,
+                'provider_id' => $providerUser->id,
+            ]);
+        } else {
+            // create a new user
+            $user = User::create([
+                'name' => $providerUser->getName(),
+                'email' => $providerUser->getEmail(),
+                'avatar' => $providerUser->getAvatar(),
+                'provider' => $driver,
+                'provider_id' => $providerUser->getId(),
+                // user can use reset password to create a password
+                'password' => ''
+            ]);
+        }
+
+        // login the user
+        Auth::login($user, true);
+
+        return $this->sendSuccessResponse();
+    }
+
+    /**
+     * Check for provider allowed and services configured
+     *
+     * @param $driver
+     * @return bool
+     */
+    private function isProviderAllowed($driver)
+    {
+        return in_array($driver, $this->providers) && config()->has("services.{$driver}");
     }
 }
