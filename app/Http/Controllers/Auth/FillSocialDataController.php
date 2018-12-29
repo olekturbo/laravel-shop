5193c5a0116030d961d3f40ae9487995a9edbfa3
olekturbo <aleks.szewczak@gmail.com>
2018-12-29 18:52:02 +0100

    Remove construct from controller

--- app/Http/Controllers/Auth/FillSocialDataController.php
+++ app/Http/Controllers/Auth/FillSocialDataController.php
@@ -8,13 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class FillSocialDataController extends Controller
 {
-    /**
-     * FillSocialDataController constructor.
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
